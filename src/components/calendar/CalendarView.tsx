ba4f52d01d8e52f163d81eb7d0a51db856783dbc
Julio Garcia <julio@gepgroup.es>
2025-09-22 10:33:11 +0200

    Adjust weekly calendar start time and scrolling

--- src/components/calendar/CalendarView.tsx
+++ src/components/calendar/CalendarView.tsx
@@ -9,24 +9,28 @@ import Card from 'react-bootstrap/Card';
 const CalendarView = () => (
   <Card className="calendar-card border-0">
     <Card.Body className="p-0">
-      <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
-        initialView="timeGridWeek"
-        headerToolbar={{
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-        }}
-        weekends
-        selectable={false}
-        editable={false}
-        slotDuration="00:30:00"
-        scrollTime="06:00:00"
-        nowIndicator
-        events={[]}
-        locales={[esLocale]}
-        locale="es"
-      />
+      <div className="calendar-scroll-container">
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
+          initialView="timeGridWeek"
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+          }}
+          weekends
+          selectable={false}
+          editable={false}
+          slotDuration="00:30:00"
+          slotMinTime="06:00:00"
+          scrollTime="06:00:00"
+          height="parent"
+          nowIndicator
+          events={[]}
+          locales={[esLocale]}
+          locale="es"
+        />
+      </div>
     </Card.Body>
   </Card>
 );
